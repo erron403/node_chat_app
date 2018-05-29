353eef0e4ed501d1e23079a9efa56acb4aad9af8
erron403 <root201613@gmail.com>
2018-05-29 07:56:30 +0530

    add socket.io and connect and disconnect event

--- server/server.js
+++ server/server.js
@@ -1,13 +1,27 @@
 const path = require('path'),
+      http = require('http'),
+      socketIO = require('socket.io'),
       express = require('express'),
       app = express();
 
 const staticFilesPath = path.join(__dirname, '../public'),
       PORT = process.env.PORT || 3000;
 
+const server = http.createServer(app),
+      io = socketIO(server);
+
 app.use(express.static(staticFilesPath));
 app.set("x-powered-by", false);
 
-app.listen(PORT, () => {
+io.on("connection", (socket) => {
+  console.log("New user is connected!");
+
+  socket.on("disconnect", () => {
+    console.log("user is disconnected!");
+  });
+  
+});
+
+server.listen(PORT, () => {
   console.log(`Server start at ${PORT}`);
 });
